739fca1a82f576c9257341b21f89b58dd7cb5001
Musa Arzu <musanday@gmail.com>
2017-11-20 21:57:50 +0100

    - Google Datastore API installed
    - insertWords - Takes array of words then insert if it is not exist on the database
    
    - getRandomWords takes number of words as parameter returns array of randomized words from datastore.

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const request = require('request');
 const Vision = require('@google-cloud/vision');
+const Datastore = require('@google-cloud/datastore');
 const bodyParser = require('body-parser');
 
 const fs = require('fs');
 
-const vision = Vision(
-    {
-        projectId: 'photobulary-elte',
-        keyFilename: 'Photobulary-Elte-45bfdaf4b523.json'
-    }
-);
+const keyfile = {
+    projectId: 'photobulary-elte',
+    keyFilename: 'Photobulary-Elte-45bfdaf4b523.json'
+}
+
+const vision = Vision(keyfile);
+const datastore = Datastore(keyfile);
 
 var app = express();
 
@@ -32,19 +34,19 @@ app.post("/processimage", (req, res) => {
     var image64Base = req.body.image64Base;
     var words = req.body.words;
 
-    if (!words || words.length == 0 ){
+    if (!words || words.length == 0) {
         errors.push("Words are required.");
     }
 
-    try{
+    try {
         isValidBase64Image(image64Base);
-    }catch(e){
+    } catch (e) {
         errors.push(e.message);
     }
-    
-    if (errors.length != 0){
+
+    if (errors.length != 0) {
         res.status(400).send({
-            errorMessage : errors
+            errorMessage: errors
         })
     }
 
@@ -52,36 +54,134 @@ app.post("/processimage", (req, res) => {
     let requests = [];
 
     let requestObj = {
-        image: {content: image64Base},
-        features: {type: 'LABEL_DETECTION'}
-      };
-    
-      requests.push(requestObj);
-      vision.batchAnnotateImages({requests: requests}).then((results) => 
-      { 
+        image: { content: image64Base },
+        features: { type: 'LABEL_DETECTION' }
+    };
+
+    requests.push(requestObj);
+    vision.batchAnnotateImages({ requests: requests }).then((results) => {
         var labelledResults = results[0].responses[0].labelAnnotations;
-        res.send(matchResults(labelledResults,words));
-      }).catch(function(reason) {
+        res.send(matchResults(labelledResults, words));
+    }).catch(function (reason) {
         // rejection
         res.status(400).send({
-            errorMessage : reason
+            errorMessage: reason
         })
-     });
+    });
 });
 
 /**
  * Returns array of strings used in game
  * @return {array[string]} words
  */
-app.get("/getwords",(req,res) => {
+app.get("/getwords", (req, res) => {
     //DUMMY RESULTS
     res.send({
-        words : ["apple","table","tree","car","cup"]
+        words: ["apple", "table", "tree", "car", "cup"]
     });
 
     //TODO - Words must be taken from a dictionary like database or file.
-    
+
 });
+
+/**
+ * Processes the image though API using image "image64Base" returns array of definitive words. 
+ * @param {string} image64Base
+ * @return {array[string]} result
+ */
+app.post("/getwordsfromimage", (req, res) => {
+
+    var image64Base = req.body.image64Base;
+    isValidBase64Image(image64Base);
+
+    let requests = [];
+
+    let requestObj = {
+        image: { content: image64Base },
+        features: { type: 'LABEL_DETECTION' }
+    };
+
+    requests.push(requestObj);
+    vision.batchAnnotateImages({ requests: requests }).then((results) => {
+        var labelledResults = results[0].responses[0].labelAnnotations;
+        var resultc = getDescriptions(labelledResults);
+        res.send(resultc);
+
+    }).catch(function (reason) {
+        // rejection
+        console.log(reason);
+        res.status(400).send({
+            errorMessage: reason
+        })
+    });
+
+});
+
+/**
+ * Processes the image though API using image "image64Base" returns array of definitive words. 
+ * @param {array[string]} words
+ * @return status code
+ */
+app.post("/insertWords", (req, res) => {
+    var words = req.body.words;
+    if (!words) {
+        "words are required."
+    }
+    words.length
+    var count = 0;
+    
+    words.forEach(element => {
+        console.log(element)
+        const kind = 'Words';
+        const name = element;
+        const wordKey = datastore.key([kind, name]);
+        datastore.save({
+            key: wordKey,
+            data: {
+                Word : element 
+            }
+        }).then(() => {
+            count++;
+            if (count == words.length) {
+                console.log("Words are successfully inserted into dictionary");
+                res.sendStatus(200);
+            }
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    });
+})
+
+/**
+ * Processes the image though API using image "image64Base" returns array of definitive words. 
+ * @param {number} words
+ * @return {array[string]} words
+ */
+app.get("/getRandomWords", (req, res) => {
+    var randomWordNum = req.query.num_words;
+
+    const query = datastore.createQuery('Words');
+
+    datastore.runQuery(query)
+    .then((results) => {
+      // Task entities found.
+      const WordsResult = results[0];
+
+
+      var words = [];
+      var resultsToReturn = [];
+      WordsResult.forEach((Word) => words.push(Word.Word));
+      for (let index = 0; index < randomWordNum; index++) {
+        var indexToTake = Math.floor(Math.random() * words.length);
+        console.log(indexToTake);
+        resultsToReturn.push(words.splice(indexToTake,1)[0]);
+      }
+      res.send(resultsToReturn);
+      
+    });
+});
+
 
 //STARTING APP
 
@@ -90,17 +190,23 @@ app.listen(3000);
 
 //INTERNAL OPERATIONS
 
-function matchResults(APIResults, words){
+function getDescriptions(labelledResults) {
+    var descriptions = [];
+    labelledResults.forEach(element => { descriptions.push(element.description) });
+    return descriptions;
+}
+
+function matchResults(APIResults, words) {
     return APIResults.diff(words);
 }
 
-Array.prototype.diff = function(arr2) {
+Array.prototype.diff = function (arr2) {
     var ret = [];
     this.sort();
     arr2.sort();
-    for(var i = 0; i < this.length; i += 1) {
-        if(arr2.indexOf( this[i].description ) > -1){
-            ret.push( this[i] );
+    for (var i = 0; i < this.length; i += 1) {
+        if (arr2.indexOf(this[i].description) > -1) {
+            ret.push(this[i]);
         }
     }
     return ret;
@@ -115,16 +221,9 @@ function base64_encode(file) {
 
 function isValidBase64Image(dataString) {
 
-    if(!dataString)
-    {
+    if (!dataString) {
         throw new Error("image64Base is required");
     }
 
-    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-
-    if (matches === null  ) {
-        throw new Error("image64Base is not a valid image data.");
-    }
-
     return true;
-  }
+}
